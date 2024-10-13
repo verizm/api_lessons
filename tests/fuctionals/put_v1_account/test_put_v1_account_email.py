7cedd237d9a924b249e61c1bf8f1622f79635a7c
verizm <myemail@mail.ru>
2024-10-13 21:28:45 +0300

    fix line

---
+++ tests/fuctionals/put_v1_account/test_put_v1_account_email.py
@@ -0,0 +1,60 @@
+import json
+import random
+import allure
+import pytest
+from http import HTTPStatus
+from models.api_models.api_account_models.post_v1_accounts_models import PostV1AccountsRequest
+from models.api_models.api_login_models.post_v1_login_models import PostV1LoginRequest
+
+
+def get_token_by_login(login: str, mails) -> str:
+    """
+    Get access token by user login.
+     """
+    for item in mails['items']:
+        user_content = json.loads(item['Content']['Body'])
+        if user_content["Login"] == login:
+            token = user_content["ConfirmationLinkUrl"].split("/")[-1]
+            return token
+        else:
+            raise Exception(f"Access token not found by login {login}")
+
+
+@pytest.mark.usefixtures("init_api_clients")
+class TestPutV1AccountEmail:
+
+    @allure.title("Check login after change email")
+    def test_post_v1_account_email(self):
+        login = f"vera{random.randrange(1000)}"
+        user = PostV1AccountsRequest(login=login, email=f"{login}@mail.ru", password="1234567889")
+        login_data = PostV1LoginRequest(login=user.login, password=user.password)
+        new_login = f"vera_new{random.randrange(1000)}"
+        user_new_data = PostV1AccountsRequest(login=user.login, email=f"{new_login}@gmail.ru", password=user.password)
+
+        with allure.step("Register new user"):
+            self.account_api.post_v1_account(user)
+            mails = self.mailhog_api.get_api_v2_messages()
+            token = get_token_by_login(user.login, mails)
+            self.account_api.put_v1_account_token(token)
+            self.login_api.post_v1_account_login(login_data)
+
+        with allure.step("Change email data"):
+            change_mail_resp = self.account_api.put_v1_account_email(user_new_data)
+            assert change_mail_resp == HTTPStatus.OK, f"Error status code after change email: {change_mail_resp}"
+
+        with allure.step("Login with new email"):
+            login_response = self.login_api.post_v1_account_login(login_data)
+            assert login_response == HTTPStatus.FORBIDDEN, \
+             f"Error status code after login under not authorized user: {login_response}"
+
+        with allure.step("Get new account token from mail"):
+            mails = self.mailhog_api.get_api_v2_messages()
+            token = get_token_by_login(user_new_data.login, mails)
+
+        with allure.step("Authorize user with new email"):
+            authorize_response = self.account_api.put_v1_account_token(token)
+            assert authorize_response == HTTPStatus.OK, f"Error status code after authorize user: {authorize_response}"
+
+        with allure.step("Login under user after change email"):
+            login_response = self.login_api.post_v1_account_login(login_data)
+            assert login_response == HTTPStatus.OK, f"Error status code after login user: {login_response}"
